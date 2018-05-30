857eb26dd783e8c363dd88d8031678cc58709742
isabelle may salmon <845868806@bun2-206-13.local>
2018-05-31 09:16:07 +1200

    created array and added audio

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ function changeColour(){
     yellowPetals[0].style.display = "block";
 };
 
-plainPetals[0].addEventListener('click', changeColour, false);
+plainPetals[0].addEventListener('mouseover', changeColour, false);
+// END test
+
+// Store audio elements into an array
+var audioArray = [];
+
+// Create audio elements with a loop and store them into audioArray
+function createAudioEl(){
+    for  (var i = 0; i < plainPetals.length; i++){
+        var audioObj = new Audio();
+        audioObj.src = 'media/audio'+i+'.wav';
+        console.log(audioObj);
+        audioArray.push(audioObj);
+    }
+}
+createAudioEl();
+console.log(audioArray);
+
+
 
 })(); //END IIFE
+
+
